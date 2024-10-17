ce9cb2b05c93db6a2f560edb478150870855ccce
Jouni Korhonen <jouni.korhonen@iki.fi>
2024-10-17 04:59:08 +0300

    Following chnages: * Added a mode to zxpac4 where the sliding window is max 32K (useful   with 8bit targets). * Added default algorithms into target configurations. * First versions of the Z80 decompressor for the zxpac4 history   parser version.
    Signed-off-by: Jouni Korhonen <jouni.korhonen@iki.fi>

---
+++ zxpac4/inc/cost4_32k.h
@@ -0,0 +1,55 @@
+/**
+ * @file cost4_32k.h
+ * @version 0.2
+ * @brief lzpac4 specific Literal & Match encoding cost calculator.
+ * @author Jouni 'Mr.Spiv' Korhonen
+ * @date 7-Apr-2024
+ * @copyright The Unlicense 
+ *
+ */
+
+#ifndef _COST4_32K_INCLUDED
+#define _COST4_32K_INCLUDED
+
+#include "lz_base.h"
+#include "mtf256.h"
+
+#ifdef MTF_CTX_SAVING_ENABLED
+#define ZXPAC4_32K_MTF_CTX_SIZE   128   // This is a bit of dirty forward knowledge how
+                                    // mtf256 works.. just to avoid dynamic memory
+                                    // allocation for all contexts..
+#endif
+
+class zxpac4_32k_cost: public lz_cost<zxpac4_32k_cost> {
+    //mtf_encode m_mtf;
+    bool m_debug;
+    bool m_verbose;
+public:
+   zxpac4_32k_cost(
+        const lz_config* p_cfg, int ins=-1, int max=-1);
+    ~zxpac4_32k_cost(void);
+
+    // Base class interface method implementations
+    int impl_literal_cost(int pos, cost* c, const char* buf);
+    int impl_match_cost(int pos, cost* c, const char* buf);
+    int impl_init_cost(cost* c, int sta, int len, int pmr);
+    cost* impl_alloc_cost(int len, int max_chain);
+    int impl_free_cost(cost* cost);
+
+    int impl_get_offset_bits(int offset);
+    int impl_get_length_bits(int length);
+    int impl_get_literal_bits(char literal, bool is_ascii);
+    
+    int impl_get_offset_tag(int offset, char& byte_tag, int& bit_tag);
+    int impl_get_length_tag(int length, int& bit_tag);
+    int impl_get_literal_tag(const char* literals, int length, char& byte_tag, int& bit_tag);
+    
+    void impl_enable_debug(bool enable) {
+        m_debug = enable;
+    }
+    void impl_enable_verbose(bool enable) {
+        m_verbose = enable;
+    }
+};
+
+#endif  // _COST4_32K_INCLUDED
