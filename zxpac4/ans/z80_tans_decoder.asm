b681bcf8f6a40fae5ef660b3f0dfe4f689c8378f
Jouni Korhonen <jounikor@gmail.com>
2025-07-28 12:56:21 +0300

    Refactored the tANS table creation to accept bit packed symbol frequencies.
    Signed-off-by: Jouni Korhonen <jounikor@gmail.com>

--- zxpac4/ans/z80_tans_decoder.asm
+++ zxpac4/ans/z80_tans_decoder.asm
@@ -11,6 +11,7 @@
 INITIAL_STATE   equ     3
 M_              equ     32
 LS_LEN          equ     8
+SYMBOL_FREQ_BITS    equ 5
 SPREAD_STEP     equ     5
 
 ; Indices to both Y and L tables.. Tables must be arranged such that
@@ -21,13 +22,25 @@ Y_              equ     M_
 L_              equ     0
 
 
-EXTRACT_SYMBOL_FREQ     MACRO   ptr
-        dec     ptr
-        ld      a,(ptr)
+EXTRACT_SYMBOL_FREQ     MACRO
+        local   not_empty_bitbuf
+        local   bitbuf_loop
+
+        xor     a
+        ld      b,SYMBOL_FREQ_BITS
+bitbuf_loop:
+        sla     d
+        jr nz,  not_empty_bitbuf
+        dec     hl
+        ld      d,(hl)
+        rl      d
+not_empty_bitbuf:
+        adc     a,a
+        djnz    bitbuf_loop
         ENDM
 
 EXTRACT_BYTE    MACRO   reg,ptr
-        dec     ptr
+        dec     hl
         ld      reg,(ptr)
         ENDM
 
@@ -43,20 +56,23 @@ main:
 ; Build decoding tables
 ;
 ; Inputs:
-;  HL = a ptr to the end of the Ls array
+;  HL = a ptr to the end of the Ls array. The array MUST have
+;       symbold frequencies in a reverse order, i.e. the last
+;       frequency is for the symbol 0 etc.
+;       The array has a predefined length LS_LEN.
 ;  IX = a ptr to generated tables. HL must be such that
 ;       the geenrated table fits into 256 bytes aligned
 ;       memory area. Start of the table can be shiftes
 ;       by L bytes.
 ;  
 ; Returns:
-;  Nothing
+;  D (bitbuffer)
 ;
 ; Changes:
 ;  HL, IX
 ;
 ; Trashes:
-;  A,B,D,E
+;  A,AF',B,D,E
 ;
 ;
 ; Note: The sum of symbol frequencies must be a power of two!
@@ -65,21 +81,23 @@ main:
 tans_build_decoding_tables:
         ld      a,INITIAL_STATE
         ld      b,LS_LEN
-        ld      e,0
+        ld      de,0x8000
 
         ; A = xp
         ; B = M i.e. the size of Ls i.e. Ls_len, which is fixed in this case.
         ; E = s
 _main_loop:
         push    bc
-        push    af
+        ex      af,af'
 
+        ; Bit extract trashes B, which is OK in this case. Bits are returned
+        ; in A and Z-flag is set accordingly to what A contains.
         EXTRACT_SYMBOL_FREQ hl
-        and     a
         jr z,   _zero_symbol
+        
         ld      b,a     ; B = counter
-        ld      d,a     ; D = p
-        pop     af
+        ld      c,a     ; C = p
+        ex      af,af'
 _per_symbol_loop:
         push    ix
         push    af
@@ -88,8 +106,8 @@ _per_symbol_loop:
         ld      ixl,a
         
         ; self.y[xp] = p
-        ld      (ix+Y_),d
-        inc     d
+        ld      (ix+Y_),c
+        inc     c
         
         ; self.L[xp] = s
         ld      (ix+L_),e
@@ -105,7 +123,7 @@ _per_symbol_loop:
 
         db      $fe
 _zero_symbol:
-        pop     af
+        ex      af,af'
         pop     bc
         inc     e
         djnz    _main_loop
@@ -160,8 +178,9 @@ _not_empty:
 ; The symbol freq table must be in reverse order !
 temp_ls_table:
         ;   7 6 5 4 3 2 1 9 and total sum of 32
-        ;db  7,6,2,0,0,9,6,2
-        db  2,6,9,0,0,2,6,7
+        ;db  2,6,9,0,0,2,6,7
+        ;db  00010001b,10010010b,00000000b,00001000b,11000111b
+        db  11000010b,00100100b,00000000b,10000100b,00111001b
 temp_ls_table_end:
 
 
