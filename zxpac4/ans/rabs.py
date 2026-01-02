72bc0222216fe8018d3799e512776a9e3ac7f422
Jouni Korhonen <jouni.korhonen@iki.fi>
2026-01-02 09:31:26 -0800

    Added a simple model update that works nicely for the end to start decoding of rABS symbols.
    Signed-off-by: Jouni Korhonen <jouni.korhonen@iki.fi>

--- zxpac4/ans/rabs.py
+++ zxpac4/ans/rabs.py
@@ -1,7 +1,7 @@
 # 
 # (c) 2025-6 Jouni 'Mr.Spiv' Korhonen
 #
-# This example code implements streaming uABS binary encoder and decoder.
+# This example code implements streaming urBS binary encoder and decoder.
 # The renormalization part has been 'optimized' for 8-bit architectures
 #
 
@@ -19,69 +19,68 @@ import math
 #  - Target decoder architecture is Z80
 #
 
+# M must be a power of two.. and in this case also fixed to 256
+M = 256
+
+# L is selected so that we can check against a 16 bit register sign bit
 L_BIT_LOW = 0x8000
 
-# Propabiliry of 0.5
-INIT_PROP_FOR_0 = int(10)
-
+# Initial propabiliry of 0.5
+INIT_PROP_FOR_0 = int(128)
 
 # Throttle model update rate
 UPDATE_RATE = 4
 
 #
-def update_propability(prop,bit):
-    print(f"update: {prop}, {bit}")
-    if (bit == 1):
-        update = (256 - prop) >> UPDATE_RATE
+def update_propability(prop: int ,bit: int ) -> int:
+	#print(f"update: {prop}, {bit}")
+	if (bit == 1):
+		update = (M - prop) >> UPDATE_RATE
             
-        if (update == 0):
-            update = 1
+		if (update == 0):
+			update = 1
 
-        prop = prop - update
+		prop = prop - update
 
-        if (prop <= 0):
-            prop = 1
+		if (prop <= 0):
+			prop = 1
+	else:
+		update = prop >> UPDATE_RATE
 
-    else:
-        update = prop >> UPDATE_RATE
+		if (update == 0):
+			update = 1
 
-        if (update == 0):
-            update = 1
+		prop = prop + update
 
-        prop = prop + update
+		if (prop >= M):
+			prop = M - 1;
 
-        if (prop > 255):
-            prop = 255;
+	#print(f"  updated: {update}, {prop}")
+	return prop
 
+#
+def encode(out: [],state: int,bit: int,prop_of_0: int ) -> int:
 
-    print(f"  updated: {update}, {prop}")
-    return prop
-
-
-def encode(out,state,bit):
 	if (bit == 0):
 		Fi = prop_of_0
 		Ci = 0
 	else:
-		Fi = 256 - prop_of_0
+		Fi = M - prop_of_0
 		Ci = prop_of_0
-
-	state_max = ((L_BIT_LOW >> 8) << 1) * Fi
+	
+	state_max = ((L_BIT_LOW // M) << 1) * Fi
 	while (state >= state_max):
-		print(f"  renorm 0x{state:04x} - {state & 1:d} ->",end=" ")
 		out.append(state & 0x01)
 		state >>= 1
-		print(f"0x{state:04x}")
 
-	new_state = ((state // Fi) << 8) + Ci + (state % Fi)
+	new_state = ((state // Fi) * M) + Ci + (state % Fi)
 	return new_state
 
-
-
-def decode(out,state):
+#
+def decode(out: [],state: int,prop_of_0: int) -> (int,int):
     # decode
-    d = state >> 8
-    r = state & 255
+    d = state // M
+    r = state & (M - 1)
 
     # s = freq_to_index(r)
     if (r < prop_of_0):
@@ -90,9 +89,9 @@ def decode(out,state):
         Is = 0
     else:
         s = 1
-        Fs = 256 - prop_of_0
+        Fs = M - prop_of_0
         Is = prop_of_0
-
+    
     # Fs = F[s]
     # Is = C[s]
 
@@ -103,39 +102,52 @@ def decode(out,state):
     # renorm
     while (new_state < L_BIT_LOW):
         b = out.pop() & 0x01
-        print(f"  renorm 0x{new_state:04x} + {b:d} ->", end=" ")
         new_state = (new_state << 1) | b
-        print(f"0x{new_state:04x}")
 
     return s,new_state
 
 if (__name__ == "__main__"):
-    S = [0,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,1,0,1,0,1,0,0,0,0]
-    O = []
-    out = []
+	S = [0,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,1,0,1,0,1,0,0,0,0]
+	O = []
+	out = []
 
 	# initialize
-    state = L_BIT_LOW
-    global prop_of_0
-    prop_of_0 = INIT_PROP_FOR_0
+	state = L_BIT_LOW
 
-    for symbol in reversed(S):
-        state = encode(out,state,symbol)
-        print(f"Input: {symbol}, state: 0x{state:x}, prop_of_0: 0x{prop_of_0:x}");
-        #prop_of_0 = update_propability(prop_of_0,symbol)
+	# Calculate propabilities.. these are done backwards since we will decode
+	# ANS encoded file from last to first symbol..
+	prop_of_0 = INIT_PROP_FOR_0
+	props = []
 
+	for symbol in reversed(S):
+		props.append(prop_of_0)
+		prop_of_0 = update_propability(prop_of_0,symbol)
 
-    print(f"Final state: 0x{state:x}, final prop_of_0: 0x{prop_of_0:x}\n")
-    print(out)
+	print("Reversed propabilities:")
+	print(props)
 
-    O = []
-    prop_of_0 = INIT_PROP_FOR_0
+	# Encode from start to end using reversed propabilities i.e. pop the last
+	# propabuility and encode the first symbol using it.
 
-    for i in range(S.__len__()):
-        symbol,state = decode(out,state)
-        print(f"Output: {symbol} and state: 0x{state:x}");
-        O.append(symbol)
-        #prop_of_0 = update_propability(prop_of_0,symbol)
+	for symbol in S:
+		prop_of_0 = props.pop()
+		state = encode(out,state,symbol,prop_of_0)
 
-    print(O)
+	print(f"Final state: 0x{state:x}")
+	print(f"o:",out)
 
+	# Decode from end to start.. state is the final and propabiloity we
+	# will update while decoding
+
+	prop_of_0 = INIT_PROP_FOR_0
+	O = []
+
+	for i in range(S.__len__()):
+		symbol,state = decode(out,state,prop_of_0)
+		O.append(symbol)
+		prop_of_0 = update_propability(prop_of_0,symbol)
+
+	print(f"O:", [o for o in reversed(O) if True])
+	print(f"S:", S)
+
+#/* vim: set tabstop=4:softtabstop=4:shiftwidth=4:noexpandtab */
